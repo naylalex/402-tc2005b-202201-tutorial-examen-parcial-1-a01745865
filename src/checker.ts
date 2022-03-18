d03ac96ccad6cea37c3a4fb2982a76ba078ab1c3
a01745865 <69483330+a01745865@users.noreply.github.com>
2022-03-18 17:11:48 +0000

    Falta que se integre ts a html

--- src/checker.ts
+++ src/checker.ts
@@ -2,12 +2,14 @@ function isPalindrome(text: string): boolean {
   console.log('isPalindrome...');
   console.log(text);
   let bPalindrome: boolean = true;
-  for (let i = 0; i < text.length; i++) {
+  for (let i:number = 0; i < text.length; i++) {
     console.log(text.charAt(i).toLowerCase(), 
       '\t', text.charAt(text.length - i - 1).toLowerCase());
       //TODO: Implementar lógica de comparación de caracteres
-
-  }
+    if(text.charAt(i).toLowerCase() != text.charAt(text.length - i -1).toLowerCase()){
+      bPalindrome = false;
+    }  
+  }  
   return bPalindrome;
 }
 export default isPalindrome;
